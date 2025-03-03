526145a184163f15fb48e99353c1d3f316238305
abedsirait <abedsirait012@gmail.com>
2025-03-03 13:16:02 +0700

    update database

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("railway", "root","IYFnGiOmRYmAQdvZMEFgnxQEfSPzEoDJ",{
-    host: "switchback.proxy.rlwy.net",
-    port: 23159,
+const db = new Sequelize("stpcontrol", "root","1918171615",{
+    host: "34.50.64.242",
     dialect: "mysql",
 })
 
